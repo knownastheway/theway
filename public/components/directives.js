eec0529e2f7d97a84f3038d5e85e701ca5431b61
Daniel Bodnar <daniel.bodnar@gmail.com>
2018-09-24 02:18:53 -0500

    Added fivetwo projects slider

--- public/components/directives.js
+++ public/components/directives.js
@@ -1,35 +1,52 @@
-'use strict';
+'use strict'
 
 /* Directives */
 
-
-angular.module('myApp.directives', []).
-directive('appVersion', ['version',
-    function(version) {
-        return function(scope, elm, attrs) {
-            elm.text(version);
-        };
+angular.module('myApp.directives', [])
+.directive('appVersion', ['version',
+  function (version) {
+    return function (scope, elm, attrs) {
+      elm.text(version)
     }
+  }
 ])
 
-
-.directive('prettyphoto', function() {
-    return function(scope, element, attrs) {
-        $("[rel^='prettyPhoto']").prettyPhoto({
-            deeplinking: false
-        });
-    }
+.directive('prettyphoto', function () {
+  return function (scope, element, attrs) {
+    $("[rel^='prettyPhoto']").prettyPhoto({
+      deeplinking: false
+    })
+  }
 })
 
-.directive('flexslider', function() {
-    return function(scope, element, attrs) {
-        $('.flexslider').flexslider({
-            animation: "slide",
-            slideshow: true,
-            slideshowSpeed: 3500,
-            animationSpeed: 1000
-        });
-    }
+.directive('flexslider', function () {
+  return function (scope, element, attrs) {
+    $('.flexslider').flexslider({
+      animation: 'slide',
+      slideshow: true,
+      slideshowSpeed: 3500,
+      animationSpeed: 1000
+    })
+  }
 })
 
+.directive('scrollable', function () {
+  return function (scope, element, attrs) {
+    $('.scrollable')
+    .scrollable({
+      circular: true,
+      speed: 550
+    })
+                .autoscroll({interval: 7000})
+                .navigator()
+    $('.item-container.cloned iframe', element).removeAttr('id')
 
+    // flexslider({
+    //   selector: '.items .item-container',
+    //   animation: 'slide',
+    //   slideshow: true,
+    //   slideshowSpeed: 3500,
+    //   animationSpeed: 1000
+    // })
+  }
+})
